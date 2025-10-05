cf39b362a145cbfb5e0772fc02f8ff02ddd704fd
Simeon <111970903+simeonnv@users.noreply.github.com>
2025-10-05 13:53:35 +0300

    some simple ui thing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import MapGL from "./Map";
 function App() {
   return (
     <div className="w-screen h-screen">
+      <div className="absolute z-10 bg-blue-500 p-2 text-white border border-black rounded-xl m-2 opacity-90 text-center">
+        <p className="opacity-100">Amount of sharks in the simulation: 300</p>
+        <p className="opacity-100">Speed: 10 tps</p>
+      </div>
       <MapGL />
     </div>
   );
